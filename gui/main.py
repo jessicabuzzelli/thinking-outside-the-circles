33446923796042ff6e0554b72c5e4d155b8fbb8b
jessica buzzelli <jessicambuzzelli@gmail.com>
2020-02-12 20:34:29 -0500

    add level = region input

--- gui/main.py
+++ gui/main.py
@@ -1,9 +1,7 @@
 from tkinter import *
 import pandas as pd
 import numpy as np
-from itertools import compress
-from sklearn.preprocessing import MinMaxScaler
-from gui.scripts.vectorize import Vectorize
+from gui.scripts.vectorize import WarehouseLevelVectors, RegionLevelVectors
 
 
 class GUI:
@@ -26,7 +24,6 @@ class GUI:
         self.root.mainloop()
 
     def get_filename(self):
-        """ get file and define it contains recognized columns """
         from tkinter.filedialog import askopenfilename
 
         self.fname = askopenfilename()
@@ -37,28 +34,22 @@ class GUI:
         if msg is not None:
             return
 
-        keep = ['legacy_division_cd',
-                'segment',
-                'legacy_product_cd',
-                'sales_channel',
-                'sales_6_mos',
+        self.set_vars()
+
+    def set_vars(self):
+        kept = ['sales_6_mos',
                 'cogs_6mos',
                 'qty_6mos',
                 'picks_6mos',
                 'net_oh',
                 'pallet_quantity',
                 # 'item_poi_days',
-                'legacy_customer_cd',
-                'core_item_flag',
                 'margin_%',
                 'net_oh_$',
                 'dioh']
 
-        if all(x in keep[:5] for x in self.df.columns):
-            return
-
-        """ set input variables for input_page screen """
-        self.field_options = ['turn_and_earn', 'revenue_6mos', 'customers_per_product'] + [x for x in self.df.columns if x in keep]
+        self.field_options = ['turn_and_earn', 'profit_6mos', 'customers_per_product'] + \
+                             [x for x in self.df.columns if x in kept]
 
         wh_options = self.df.legacy_division_cd.unique().astype(str)
 
@@ -71,10 +62,15 @@ class GUI:
         if self.segment_options is None:
             return
 
+        self.level_options = ['warehouse', 'region']  # add hub?
+        self.region_options = np.append(['All'], self.df['legacy_system_cd'].unique())
+
         self.wh_var = StringVar(self.root, value='All')
         self.segment_var = StringVar(self.root, value=self.segment_options[0])
         self.cutoff_var = StringVar(self.root, value='20')
         self.field_var = []
+        self.region_var = StringVar(self.root, value='All')
+        self.level_var = StringVar(self.root)
 
         self.input_page()
 
@@ -93,8 +89,9 @@ class GUI:
         Label(frame, text="Select segment: ").grid(row=1, column=0, pady=10)
         OptionMenu(frame, self.segment_var, *self.segment_options).grid(row=1, column=1, pady=10)
 
-        Label(frame, text="Select warehouse(s): ").grid(row=2, column=0, pady=10)
-        OptionMenu(frame, self.wh_var, *self.wh_options).grid(row=2, column=1, pady=10)
+        Label(frame, text="Select scope and press REFRESH: ").grid(row=2, column=0, pady=10)
+        OptionMenu(frame, self.level_var, *self.level_options).grid(row=2, column=1, pady=10)
+        Button(frame, text='REFRESH', command=self.popup_options).grid(row=2, column=2, pady=10)
 
         Label(frame, text=" % core products: ").grid(row=3, column=0, pady=10)
         Entry(frame, textvariable=self.cutoff_var).grid(row=3, column=1, pady=10)
@@ -110,7 +107,7 @@ class GUI:
 
             txt = self.field_options[idx]
             b = Checkbutton(frame, text=txt + '  ' * (pad - len(txt)), variable=var, anchor="w")
-            b.grid(row=4 + (idx // 2), column=((idx + 1) % 2) + 1, pady=10)
+            b.grid(row=4 + (idx // 2), column=(idx % 2) + 1, pady=10)
             self.field_var.append(var)
 
         self.last_row = 3 + (len(self.field_options) // 2)
@@ -118,6 +115,31 @@ class GUI:
         frame.grid(row=4, column=len(self.field_options))
 
         define.mainloop()
+
+    def popup_options(self):
+        try:
+            self.wh_label.destroy()
+            self.wh_optionmenu.destroy()
+        except AttributeError:
+            pass
+
+        try:
+            self.region_label.destroy()
+            self.region_optionmenu.destroy()
+        except AttributeError:
+            pass
+
+        if self.level_var.get() == 'warehouse':
+            self.wh_label = Label(self.input_frame, text="Select warehouse(s): ")
+            self.wh_label.grid(row=2, column=3, pady=10)
+            self.wh_optionmenu = OptionMenu(self.input_frame, self.wh_var, *self.wh_options)
+            self.wh_optionmenu.grid(row=2, column=4, pady=10)
+
+        elif self.level_var.get() == 'region':
+            self.region_label = Label(self.input_frame, text="Select region(s): ")
+            self.region_label.grid(row=2, column=3, pady=10)
+            self.region_optionmenu = OptionMenu(self.input_frame, self.region_var, *self.region_options)
+            self.region_optionmenu.grid(row=2, column=4, pady=10)
 
     def check_inputs(self):
         try:
@@ -133,11 +155,18 @@ class GUI:
     def format_vars(self):
         self.field_var = [x.get() for x in self.field_var]
 
-        for var in [self.cutoff_var, self.segment_var, self.wh_var]:
-            var = var.get()
+        self.cutoff_var = self.cutoff_var.get()
+        self.segment_var = self.segment_var.get()
+        self.wh_var = self.wh_var.get()
+        self.region_var = self.region_var.get()
+        self.level_var = self.level_var.get()
 
-        # if self.wh_var == 'All':
-        #     self_wh_var = self.wh_options[1:]
+        print(self.wh_var)
+        params = [self.segment_var, self.field_var, self.field_options, self.cutoff_var, self.df, self.fname]
+        if self.level_var == 'warehouse':
+            self.model = WarehouseLevelVectors(self.wh_var, *params)
+        elif self.level_var == 'region':
+            self.model = RegionLevelVectors(self.region_var, *params)
 
     def output_page(self):
         self.define.withdraw()
@@ -148,21 +177,12 @@ class GUI:
         frame = Frame(outputs)
         # self.outputs.geometry('500x500')
 
-        """ RUN BACKEND """
-        model = Vectorize(wh=self.wh_var.get(),
-                          segment=self.segment_var,
-                          fields=self.field_var,
-                          field_options=self.field_options,
-                          cutoff=self.cutoff_var.get(),
-                          df=self.df,
-                          fname=self.fname)
-
         label = Label(frame, text="Success!").grid(row=0, column=0, pady=10)
-        Button(frame, text="Download new Excel sheet", command=model.export).grid(row=1, column=0, pady=10)
+        Button(frame, text="Export to Excel", command=self.model.export).grid(row=1, column=0, pady=10)
 
         text = Text(frame)
         text.grid(row=2, column=0, pady=0)
-        text.insert(INSERT, model.string_output())
+        text.insert(INSERT, self.model.string_output())
 
         frame.grid(row=2, column=2)
 
@@ -173,7 +193,7 @@ class GUI:
         if ext == 'xlsx':
             try:
                 self.df = pd.read_excel(self.fname, sheet_name='Sheet1')
-            except:  # TODO - run test on xlsx w/o multiple sheets and catch error
+            except:
                 self.df = pd.read_excel(self.fname)
         elif ext == '.csv':
             self.df = pd.read_csv(self.fname)
@@ -188,6 +208,7 @@ class GUI:
             pass
 
         keep = ['legacy_division_cd',
+                'legacy_system_cd',
                 'segment',
                 'legacy_product_cd',
                 'sales_channel',
@@ -215,17 +236,6 @@ class GUI:
 
         self.df[continuous_labels] = self.df[continuous_labels][(self.df[continuous_labels] > 0)].fillna(0)
 
-
-        # TODO -- figure out why it isn't touching the last 5 columns
-        # scaler = MinMaxScaler()
-        #
-        # out = scaler.fit_transform(self.df[continuous_labels])
-        # df2 = pd.DataFrame(out, columns=continuous_labels)
-        #
-        # for column in df2.columns:
-        #     self.df[column] = df2[column]
-        #
-        # self.df.fillna(0)
         self.df[continuous_labels] = self.df[continuous_labels].apply(self.get_max, axis=0)
 
     def get_max(self, col):
