39f225829da9604148de5e9f849f69c04a8d16bd
jessica buzzelli <jessicambuzzelli@gmail.com>
2020-02-19 13:15:10 -0500

    fix mainloop() and method call to run "concurrently"

--- gui/main.py
+++ gui/main.py
@@ -7,9 +7,9 @@ from gui.scripts.vectorize import WarehouseLevelVectors, RegionLevelVectors
 class GUI:
 
     def __init__(self):
-        self.home_page()
+        self.home()
 
-    def home_page(self):
+    def home(self):
         self.root = Tk()
         self.root.title("Click UPLOAD to begin")
         self.root.config(bg="white")
@@ -24,46 +24,102 @@ class GUI:
         self.root.mainloop()
 
     def get_filename(self):
+        self.root.withdraw()
+
         from tkinter.filedialog import askopenfilename
-
         self.fname = askopenfilename()
-        if not self.fname:
+
+        if self.fname is None:
             return
 
-        msg = self.check_file()
-        if msg is not None:
-            return
+        loading = Toplevel()
+        self.loading = loading
+        loading.title("Loading...")
+        loading.config(bg="white")
+        frame = Frame(loading)
 
-        self.set_vars()
+        Label(frame, text='Loading...').grid(row=2, column=2, pady=100, padx=100)
+        frame.grid(row=4, column=4)
 
-    def set_vars(self):
-        kept = ['sales_6_mos',
+        self.loading.after(200, self.check_read)
+        loading.mainloop()
+
+    def check_read(self):
+        failed_read = self.read_file()
+        if failed_read is True:
+            self.get_filename()
+        else:
+            failed_set_var = self.set_vars()
+            if failed_set_var is True:
+                self.get_filename()
+            else:
+                self.input_page()
+
+    def read_file(self):
+        ext = self.fname.split('.')[1]
+        if ext == 'xlsx':
+            try:
+                self.df = pd.read_excel(self.fname, sheet_name='Sheet1')
+            except:
+                self.df = pd.read_excel(self.fname)
+        elif ext == '.csv':
+            self.df = pd.read_csv(self.fname)
+        else:
+            return 'File extension not valid. Select another file.'
+
+        self.df.columns = [c.replace(' ', '_').lower() for c in self.df.columns]
+
+        try:
+            self.df = self.df[self.df['sales_channel'] == 'Warehouse']
+        except KeyError:
+            pass
+
+        keep = ['legacy_division_cd',
+                'legacy_system_cd',
+                'segment',
+                'legacy_product_cd',
+                'sales_channel',
+                'sales_6_mos',
                 'cogs_6mos',
                 'qty_6mos',
                 'picks_6mos',
                 'net_oh',
-                'pallet_quantity',
-                # 'item_poi_days',
+                'legacy_customer_cd',
+                'core_item_flag',
                 'margin_%',
                 'net_oh_$',
                 'dioh']
 
+        self.df = self.df[keep].fillna(0).replace('-', 0)
+
+    def set_vars(self):
+        options = ['sales_6_mos',
+                   'cogs_6mos',
+                   'qty_6mos',
+                   'picks_6mos',
+                   'net_oh',
+                   'pallet_quantity',
+                   'margin_%']
+
         self.field_options = ['turn_and_earn', 'profit_6mos', 'customers_per_product'] + \
-                             [x for x in self.df.columns if x in kept]
+                             [x for x in self.df.columns if x in options]
 
         wh_options = self.df.legacy_division_cd.unique().astype(str)
 
         if wh_options is None:
-            return
-
-        self.wh_options = np.append(['All'], wh_options)
+            return 'no warehouses'
 
         self.segment_options = self.df.segment.unique()
         if self.segment_options is None:
-            return
+            return 'no segments'
 
-        self.level_options = ['warehouse', 'region']  # add hub?
-        self.region_options = np.append(['All'], self.df['legacy_system_cd'].unique())
+        self.region_options = self.df['legacy_system_cd'].unique()
+        if self.region_options is None:
+            return 'no regions'
+
+        self.level_options = ['warehouse', 'region']  # TODO - check to see if add hub
+        self.region_options = np.append(['All'], self.region_options)
+        self.wh_options = np.append(['All'], wh_options)
 
         self.wh_var = StringVar(self.root, value='All')
         self.segment_var = StringVar(self.root, value=self.segment_options[0])
@@ -72,10 +128,9 @@ class GUI:
         self.region_var = StringVar(self.root, value='All')
         self.level_var = StringVar(self.root)
 
-        self.input_page()
-
     def input_page(self):
-        self.root.withdraw()
+        self.loading.withdraw()
+
         define = Toplevel()
         self.define = define
         define.title("Define inputs")
@@ -83,8 +138,8 @@ class GUI:
         frame = Frame(define)
         self.input_frame = frame
 
-        Label(frame, text="Modify model inputs below and click RUN. ").grid(row=0, column=0, pady=10)
-        Button(frame, text="RUN", width=8, command=self.check_inputs).grid(row=0, column=1, pady=10)
+        Label(frame, text="Modify model inputs below and click RUN. ").grid(row=0, column=1, pady=10)
+        Button(frame, text="RUN", width=8, command=self.check_inputs).grid(row=0, column=2, pady=10)
 
         Label(frame, text="Select segment: ").grid(row=1, column=0, pady=10)
         OptionMenu(frame, self.segment_var, *self.segment_options).grid(row=1, column=1, pady=10)
@@ -142,42 +197,77 @@ class GUI:
             self.region_optionmenu.grid(row=2, column=4, pady=10)
 
     def check_inputs(self):
+        err = False
         try:
             float(self.cutoff_var.get())
-            self.format_vars()
-            self.output_page()
 
         except ValueError:
-            Label(self.input_frame,text='ERROR. Enter a numeric value for % core products.')\
+            err = True
+
+        if err:
+            Label(self.input_frame, text='ERROR. Enter a numeric value for % core products.') \
                 .grid(row=self.last_row + 1, column=0)
             return
 
+        else:
+            self.format_vars()
+            self.loading_page2()
+
     def format_vars(self):
-        self.field_var = [x.get() for x in self.field_var]
+        field_var = [x.get() for x in self.field_var]
 
-        self.cutoff_var = self.cutoff_var.get()
-        self.segment_var = self.segment_var.get()
-        self.wh_var = self.wh_var.get()
-        self.region_var = self.region_var.get()
-        self.level_var = self.level_var.get()
+        cutoff_var = self.cutoff_var.get()
+        segment_var = self.segment_var.get()
+        wh_var = self.wh_var.get()
+        region_var = self.region_var.get()
+        level_var = self.level_var.get()
 
-        print(self.wh_var)
-        params = [self.segment_var, self.field_var, self.field_options, self.cutoff_var, self.df, self.fname]
-        if self.level_var == 'warehouse':
-            self.model = WarehouseLevelVectors(self.wh_var, *params)
-        elif self.level_var == 'region':
-            self.model = RegionLevelVectors(self.region_var, *params)
+        try:
+            wh_var = [int(wh_var)]
+            # print(self.wh_var)
+
+        except ValueError:
+            assert wh_var == 'All'
+            wh_var = self.df['legacy_division_cd'].unique()
+
+        params = [segment_var, field_var, self.field_options, cutoff_var, self.df, self.fname]
+
+        if level_var == 'warehouse':
+            self.model = WarehouseLevelVectors(wh_var, *params)
+
+        elif level_var == 'region':
+            self.model = RegionLevelVectors(region_var, *params)
+
+    def loading_page2(self):
+        loading2 = Toplevel()
+        self.loading2 = loading2
+        loading2.title("Loading...")
+        loading2.config(bg="white")
+        frame = Frame(loading2)
+
+        Label(frame, text='Loading...').grid(row=2, column=2, pady=100, padx=100)
+        frame.grid(row=4, column=4)
+
+        self.loading2.after(200, self.move_on)
+        loading2.mainloop()
+
+    def move_on(self):
+        self.model.run()
+        self.output_page()
 
     def output_page(self):
-        self.define.withdraw()
+        self.loading2.withdraw()
+
         outputs = Toplevel()
         self.outputs = outputs
         outputs.title("Outputs")
         outputs.config(bg="white")
         frame = Frame(outputs)
-        # self.outputs.geometry('500x500')
 
-        label = Label(frame, text="Success!").grid(row=0, column=0, pady=10)
+        # print(self.field_options)
+        # print(self.field_var)
+
+        Label(frame, text="Success!").grid(row=0, column=0, pady=10)
         Button(frame, text="Export to Excel", command=self.model.export).grid(row=1, column=0, pady=10)
 
         text = Text(frame)
@@ -187,59 +277,6 @@ class GUI:
         frame.grid(row=2, column=2)
 
         outputs.mainloop()
-
-    def check_file(self):
-        ext = self.fname.split('.')[1]
-        if ext == 'xlsx':
-            try:
-                self.df = pd.read_excel(self.fname, sheet_name='Sheet1')
-            except:
-                self.df = pd.read_excel(self.fname)
-        elif ext == '.csv':
-            self.df = pd.read_csv(self.fname)
-        else:
-            return 'File extension not valid. Select another file.'
-
-        self.df.columns = [c.replace(' ', '_').lower() for c in self.df.columns]
-
-        try:
-            self.df = self.df[self.df['sales_channel'] == 'Warehouse']
-        except KeyError:
-            pass
-
-        keep = ['legacy_division_cd',
-                'legacy_system_cd',
-                'segment',
-                'legacy_product_cd',
-                'sales_channel',
-                'sales_6_mos',
-                'cogs_6mos',
-                'qty_6mos',
-                'picks_6mos',
-                'net_oh',
-                'legacy_customer_cd',
-                'core_item_flag',
-                'margin_%',
-                'net_oh_$',
-                'dioh']
-
-        self.df = self.df[keep].fillna(0).replace('-', 0)
-
-        continuous_labels = ['sales_6_mos',
-                             'qty_6mos',
-                             'cogs_6mos',
-                             'margin_%',
-                             'picks_6mos',
-                             'net_oh',
-                             'net_oh_$',
-                             'dioh']
-
-        self.df[continuous_labels] = self.df[continuous_labels][(self.df[continuous_labels] > 0)].fillna(0)
-
-        self.df[continuous_labels] = self.df[continuous_labels].apply(self.get_max, axis=0)
-
-    def get_max(self, col):
-        return col / max(col)
 
 
 if __name__ == "__main__":
